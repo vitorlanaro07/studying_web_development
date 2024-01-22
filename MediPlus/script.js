9c852f5c57d280335b14b6a182b135f57da8d7f6
Vitor S. Lanaro <vitor.lanaro@hotmail.com>
2024-01-22 20:45:40 -0300

    Updating! SliderShow and Dropdown menu

--- MediPlus/script.js
+++ MediPlus/script.js
@@ -1,2 +1,41 @@
 var button_pages = document.getElementsByClassName('dropdown');
-console.log(button_pages);
+
+
+function show_list(element){
+        element.children[1].classList.toggle("showing");
+}
+
+function remove_list(element){
+    element.children[1].classList.toggle("showing");
+}
+
+var slideIndex = 1;
+
+function plus_divs(n){
+    show_slider(slideIndex += n)
+}
+
+function show_slider(n){
+    var i;
+    var x = document.getElementsByClassName("slider");
+
+    console.log(n, x.length);
+
+    if (n < x.length){
+        slideIndex = n;
+    }
+    if (n > x.length){
+        slideIndex = 1;
+    }
+
+    if (n < 1){
+        slideIndex = x.length;
+    }
+
+    for (i = 0; i < x.length; i++){
+        x[i].style.display = 'none';
+        x[slideIndex-1].style.display = 'block';
+    }
+}
+
+show_slider(slideIndex);
